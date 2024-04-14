1ceead67f043cdd953a1506b17eb566d78d74b3f
tqt <tohutieu448@gmail.com>
2024-04-14 13:20:44 +0700

    add css header 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,30 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '@/assets/images';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
+console.log(images.logo);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="logo clone from Tiktok"></img>
+                <div className={cx('search')}>
+                    <input placeholder="Search" spellCheck={false}></input>
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
